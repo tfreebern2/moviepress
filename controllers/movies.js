8ff0c969a7914825c8df917b0a5611f4bf8044e2
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-11 19:25:11 -0400

    Loop through movies on movie index page. Added HTML formatting

--- controllers/movies.js
+++ controllers/movies.js
@@ -7,8 +7,11 @@ router
   const popularMoviesURL = `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
   axios.get(popularMoviesURL)
   .then((api_res) => {
-    console.log(api_res);
     res.render('application', {
+      locals: {
+        movies: api_res.data,
+        user: req.user
+      },
       partials: {
         yield: 'views/movies/index.html'
       }
@@ -17,6 +20,6 @@ router
   .catch((err) => {
     return console.log(err);
   });
-});
+})
 
 module.exports = router;
