93da836ecabefb49ade37b97b5341eb6cb78080f
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-11 18:04:59 -0400

    Added movies and index controller. Restructred routing.

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(require('./controllers'));
 
 const port = process.env.PORT || 3000;
 
