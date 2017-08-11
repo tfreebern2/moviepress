6f28e234cd9ab0859d08f6b731ffc0125655889f
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-11 18:40:36 -0400

    Added API Key

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const es6Renderer = require('express-es6-template-engine');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('./env.js');
+}
+
 app.engine('html', es6Renderer);
 app.set('views', 'views');
 app.set('view engine', 'html');
