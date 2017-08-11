7ba74ab60ea8eb724f3ad5a65408ca047ef56d2f
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-11 18:57:29 -0400

    Added API call method on GET request

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 router
 .get('/', (req, res) => {
-  res.render('application', {
-    partials: {
-      yield: 'views/movies/index.html'
-    }
+  const popularMoviesURL = `http://api.themoviedb.org/3/movie/popular?api_key=${process.env.MOVIE_API_KEY}&language=en-US&page=1`;
+  axios.get(popularMoviesURL)
+  .then((api_res) => {
+    console.log(api_res);
+    res.render('application', {
+      partials: {
+        yield: 'views/movies/index.html'
+      }
+    });
+  })
+  .catch((err) => {
+    return console.log(err);
   });
 });
 
