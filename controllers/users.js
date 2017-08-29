db66b8cf399ec1d9de924e5ae344cc39c4e52421
Timothy Freebern II <tfreebern2@gmail.com>
2017-08-29 14:00:37 -0400

    Added Cookies to Log in Session. Added Sign Out Link/Path.

--- controllers/users.js
+++ controllers/users.js
@@ -3,20 +3,30 @@ const router = express.Router();
 const models = require('../models');
 
 router
-.get('/sign-up', (req, res) => {
-  res.render('application', {
-    partials: {
-      yield: 'views/users/sign-up.html'
-    }
-  });
-})
-.post('/sign-up', (req, res) => {
-  models.User.create(req.body, { fields: ['username', 'email', 'password'] })
-  .then((user) => {
-    res.send(JSON.stringify(req.body));
-  }).catch((error) => {
-    res.status(500);
-  });
-});
+	.get('/sign-up', (req, res) => {
+		res.render('application', {
+			partials: {
+				yield: 'views/users/sign-up.html'
+			}
+		});
+	})
+	.post('/sign-up', (req, res) => {
+		models.User
+			.create(req.body, { fields: ['username', 'email', 'password'] })
+			.then(user => {
+				res.cookie('movie_press_token', user.id, {
+					httpOnly: true,
+					maxAge: 86400000
+				});
+				res.redirect('/');
+			})
+			.catch(error => {
+				res.status(500);
+			});
+	})
+	.get('/sign-out', (req, res) => {
+		res.clearCookie('movie_press_token');
+		res.redirect('/');
+	});
 
 module.exports = router;
